def857a886fc9da6c8974ccbd825a95d61250098
faizscripts <4faizahmed@gmail.com>
2025-10-06 18:16:34 +0300

    Configure vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { AppProvider } from './context/AppContext.tsx';
@@ -11,5 +12,6 @@ createRoot(document.getElementById('root')!).render(
       <AppProvider>
           <App />
       </AppProvider>
+      <Analytics />
   </StrictMode>,
 );
